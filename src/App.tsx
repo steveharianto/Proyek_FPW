e124b1f21b569f8e8dff84aa16f0ee67a4ab28a7
steveharianto <steveharianto@gmail.com>
2023-10-05 08:59:51 +0700

    Initialize Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
-    return <div className=" bg-red-500">Hello World</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} /> */}
+                <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
